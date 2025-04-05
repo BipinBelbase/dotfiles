ea6a55008044b8aad28203606e10ffa74b973681
Bipin Belbase <bipinbelbase007@gmail.com>
2025-04-05 14:12:19 +0900

    after adding nvim ghostty zsh

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx", -- React (JSX/TSX)
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "vim",
+        "yaml",
+        "java",
+      },
+    },
+  },
+}
