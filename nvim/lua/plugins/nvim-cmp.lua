84c88addee087cf5215f25168642f1b3abb78dc8
Bipin Belbase <bipinbelbase007@gmail.com>
2025-04-06 18:24:14 +0900

    welcome professor

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -61,7 +61,7 @@ return {
 
         documentation = {
           auto_show = true,
-          auto_show_delay_ms = 200,
+          auto_show_delay_ms = 400,
         },
 
         ghost_text = { enabled = false },
