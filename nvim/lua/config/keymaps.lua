6080c17983e73533a0edd9db7f488949f50c2db6
Bipin Belbase <bipinbelbase007@gmail.com>
2025-06-15 09:09:34 +0900

    enter for c-d adn backspace for d-u

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -54,6 +54,12 @@ vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+--greatest remap
+
+vim.keymap.set("n", "<CR>", "<C-d>zz", { noremap = true }) -- Enter scrolls down
+vim.keymap.set("n", "<BS>", "<C-u>zz", { noremap = true }) -- Backspace scrolls up
+vim.keymap.set("v", "<CR>", "<C-d>zz", { noremap = true }) -- Enter scrolls down
+vim.keymap.set("v", "<BS>", "<C-u>zz", { noremap = true }) -- Backspace scrolls up
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -61,9 +67,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_next, { desc = "Next ERROR LIST" })
